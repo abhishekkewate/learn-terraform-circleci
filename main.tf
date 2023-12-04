a4da18157aee5a59209bc608484b1dcf7a7f8626
Abhishek Kewate <92569209+abhishekkewate@users.noreply.github.com>
2023-12-04 16:33:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,67 +1,46 @@
-provider "aws" {
-  region = var.region
 
-  default_tags {
-    tags = {
-      hashicorp-learn = "circleci"
-    }
-  }
-}
+ provider "aws" {
+   region = var.region
 
-resource "random_uuid" "randomid" {}
+   default_tags {
+     tags = {
+       hashicorp-learn = "circleci"
+     }
+   }
+ }
 
-resource "aws_s3_bucket" "app" {
-  tags = {
-    Name          = "App Bucket"
-    public_bucket = true
-  }
+ resource "random_uuid" "randomid" {}
 
-  bucket        = "${var.app}.${var.label}.${random_uuid.randomid.result}"
-  force_destroy = true
-}
+ resource "aws_s3_bucket" "app" {
+   tags = {
+     Name = "App Bucket"
+   }
 
-resource "aws_s3_bucket_ownership_controls" "app" {
-  bucket = aws_s3_bucket.app.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
+   bucket        = "${var.app}.${var.label}.${random_uuid.randomid.result}"
+   force_destroy = true
+ }
 
-resource "aws_s3_bucket_public_access_block" "app" {
-  bucket = aws_s3_bucket.app.id
+ resource "aws_s3_bucket_object" "app" {
+   acl          = "public-read"
+   key          = "index.html"
+   bucket       = aws_s3_bucket.app.id
+   content      = file("./assets/index.html")
+   content_type = "text/html"
+ }
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+ resource "aws_s3_bucket_acl" "bucket" {
+   bucket = aws_s3_bucket.app.id
+   acl    = "public-read"
+ }
 
-resource "aws_s3_bucket_acl" "app" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.app,
-    aws_s3_bucket_public_access_block.app,
-  ]
+ resource "aws_s3_bucket_website_configuration" "terramino" {
+   bucket = aws_s3_bucket.app.bucket
 
-  bucket = aws_s3_bucket.app.id
-  acl    = "public-read"
-}
+   index_document {
+     suffix = "index.html"
+   }
 
-resource "aws_s3_bucket_website_configuration" "app" {
-  bucket = aws_s3_bucket.app.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-resource "aws_s3_object" "app" {
-  acl          = "public-read"
-  key          = "index.html"
-  bucket       = aws_s3_bucket.app.id
-  content      = file("./assets/index.html")
-  content_type = "text/html"
-}
+   error_document {
+     key = "error.html"
+   }
+ }
